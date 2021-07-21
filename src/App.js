a347b3b6a5fd99c8cfb0d8283e8797305c21d2e4
israfil <israfil3019@gmail.com>
2021-07-22 00:48:10 +0300

    add navbar comps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import "./App.css"
 
 const App = () => {
-  return <div className="App"></div>;
-};
+  return (
+    <div className="App">
+      
+      <Navbar/>
+
+    </div>
+  );
+}
 
 export default App;
